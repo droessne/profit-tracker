2fc848e2ee2f340095f7d2ba3b0ccaa57fcb279f
Daniel Roessner <daniel.roessner@dersllc.com>
2019-01-18 19:06:12 +0000

    Update trades.sql

--- trades.sql
+++ trades.sql
@@ -1,15 +1,15 @@
 CREATE TABLE IF NOT EXISTS `trades` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `executed_date` date NOT NULL,
-  `type` enum(`Entry`,`Exit`),
+  `type` enum('Entry','Exit') CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `symbol` varchar(10) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `trade_strategy` enum(`Call`,`Call Spread`,`Put`,`Put Spread`),
-  `order_type` enum(`Buy Open`,`Sell Close`,`Sell Open`,`Buy Close`),
+  `trade_strategy` enum('Call','Call Spread','Put','Put Spread') CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `order_type` enum('Buy Open','Sell Close','Sell Open','Buy Close') CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `qty` int NOT NULL,
   `expire_date` date NOT NULL,
   `strike_price` float NOT NULL,
   `executed_price` float NOT NULL,
-  `order_type2` enum(`Buy Open`,`Sell Close`,`Sell Open`,`Buy Close`),
+  `order_type2` enum('Buy Open','Sell Close','Sell Open','Buy Close') CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `strike_price2` float NOT NULL,
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
