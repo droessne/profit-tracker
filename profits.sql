715cae942183d6c0c029453242fa52f88e3ec1e2
Daniel Roessner <daniel.roessner@dersllc.com>
2019-01-24 02:50:27 +0000

    Update profits.sql

--- profits.sql
+++ profits.sql
@@ -5,6 +5,6 @@ CREATE TABLE IF NOT EXISTS `profits` (
   `amount` float,
   `platform` enum('Alpha-9','Money Calendar Pro') CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `entry_id` int(11),
-  `exit_id` int(11),
+  `exit_id` varchar(50),
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
