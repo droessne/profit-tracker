f442461d143ee7a65585ac67913ffd7454fdf4dc
Daniel Roessner <daniel.roessner@dersllc.com>
2019-02-08 15:56:30 +0000

    Update add_entry_trade_db.php

--- add_entry_trade_db.php
+++ add_entry_trade_db.php
@@ -28,7 +28,7 @@ if ($dbconnection->connect_error) {
   } elseif ('Call Spread' == $trade_strategy) {
     $total = ((((floatval($qty) * 100) * floatval($executed_price)) * -1) + $com_fee);
   } elseif ('Put Spread' == $trade_strategy) {
-    $total1 = ((((floatval($qty) * 100) * floatval($executed_price)) * -1) + $com_fee);
+    $total = (((floatval($qty) * 100) * floatval($executed_price)) + $com_fee);
   }
 
   if ($trade_strategy == 'Call'){
