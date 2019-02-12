d33a8e4b37926ce8a3af4689b71ba2559842e2ed
Daniel Roessner <daniel.roessner@dersllc.com>
2019-02-12 23:59:57 +0000

    Update defaults.cfg.php

--- include/defaults.cfg.php
+++ include/defaults.cfg.php
@@ -1,3 +1,3 @@
 <?php
-$platforms = array("Alpha-9", "Straight Line Profits", "Money Calendar Pro", "Weekly Money Call", "The Money Zone", "Fast Fortune Club", "10 Minute Millionaire");
+$platforms = array("Alpha-9", "Straight Line Profits", "Money Calendar Pro", "Weekly Money Call", "The Money Zone", "Fast Fortune Club", "10 Minute Millionaire", "Stealth Profits Trader", "Seismic Profits Alert");
 ?>
