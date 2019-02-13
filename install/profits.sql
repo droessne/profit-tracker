1706373805420f133ccd6b0866ecf175cc5586fb
Daniel Roessner <daniel.roessner@dersllc.com>
2019-02-13 00:16:53 +0000

    Update profits.sql

--- install/profits.sql
+++ install/profits.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS `profits` (
   `date` date NOT NULL,
   `description` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `amount` float,
-  `platform` enum('Alpha-9','Money Calendar Pro','Straight Line Profits','Weekly Money Call','The Money Zone','Fast Fortune Club','10 Minute Millionaire','Stealth Profits Trader','Seismic Profits Alert') CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `platform` enum('Alpha-9','Money Calendar Pro','Straight Line Profits','Weekly Money Call','The Money Zone','Fast Fortune Club','10 Minute Millionaire','Stealth Profits Trader','Seismic Profits Alert','Deposit') CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `entry_id` int(11),
   `exit_id` varchar(50),
   PRIMARY KEY (`ID`)
