bd65f28646fcff27b73586cfe7fbf4348d40568f
Daniel Roessner <daniel.roessner@dersllc.com>
2019-02-15 19:06:34 +0000

    Add new file

---
+++ view_closed_form.php
@@ -0,0 +1,32 @@
+<html>
+<title> Profit Tracker </title>
+<body>
+<H1> View Closed Trades </H1>
+<form action="view_closed.php" method="post">
+<table>
+<tr>
+<td><label for="platform">Platform:</label></td>
+<td><select id="platform" name="platform">
+<?php
+  require_once("include/defaults.cfg.php");
+  foreach ($platforms as &$p) {
+    echo '<option value="'.$p.'">'.$p.'</option>';
+  }
+?>
+</select></td>
+</tr><tr>
+<td><label for="time_frame">Time Frame:</label></td>
+<td><select id="time_frame" name="time_frame">
+<option value="all">All</option>
+<option value="last_month">Last Month</option>
+<option value="last_week">Last Week</option>
+<option value="ytd">YTD</option>
+</select></td>
+</tr>
+</table>
+
+<input type="submit" />
+</form>
+
+</body>
+</html>
