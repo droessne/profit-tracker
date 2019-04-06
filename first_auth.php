b5a7c184cefb51f9ae403c9b73f03d502521509c
Daniel Roessner <daniel.roessner@dersllc.com>
2019-04-06 18:56:44 +0000

    Update first_auth.php

---
+++ first_auth.php
@@ -0,0 +1,18 @@
+<html>
+<title> First Auth Input </title>
+<body>
+<H1> First Auth Input </H1>
+<form action="first_auth_db.php" method="post">
+<table border=1>
+<tr>
+<th>JSON</th>
+</tr><tr>
+<td><input type="textarea" name="json" value="" size="500"></td>
+</tr>
+</table>
+
+<input type="submit" />
+</form>
+
+</body>
+</html>
