63fc6a646aed4a7c121f1f6b9bf59ad02f843045
Daniel Roessner <daniel.roessner@dersllc.com>
2019-04-06 19:19:33 +0000

    Update auth.sql

--- install/auth.sql
+++ install/auth.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS `auth` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `refresh_token` varchar(500) NOT NULL,
+  `refresh_token` varchar(2500) NOT NULL,
   `client_id` varchar(13),
   `redirect_uri` varchar(50),
-  `access_token` varchar(500),
+  `access_token` varchar(2500),
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
