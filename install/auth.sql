f711bcf2c82f3b68a1238f62eb60584465f76f90
Daniel Roessner <daniel.roessner@dersllc.com>
2019-04-06 18:46:29 +0000

    Add new file

---
+++ install/auth.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS `auth` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `refresh_token` varchar(500) NOT NULL,
+  `client_id` varchar(13),
+  `redirect_uri` varchar(50),
+  `access_token` varchar(500),
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
