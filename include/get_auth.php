1a3c46bb485d28d9069a14000d7a915f4a9614d7
Daniel Roessner <daniel.roessner@dersllc.com>
2019-04-06 20:45:19 +0000

    Update get_auth.php

--- include/get_auth.php
+++ include/get_auth.php
@@ -1,22 +1,32 @@
 <?php
 //https://auth.tdameritrade.com/oauth?client_id=DERS_MONEY@AMER.OAUTHAP&response_type=code&redirect_uri=https://money.dersllc.com:8743
 
-require_once("include/database.cfg.php");
+require_once("database.cfg.php");
 
 function get_auth() {
     $dbconnection = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
     if (!$dbconnection->connect_errno) {
-        #$sql = "SELECT * FROM auth WHERE type='Entry' AND platform='".$platform."';";
+        $sql = "SELECT * FROM auth WHERE ID='1';";
         $results = $dbconnection->query($sql);
         while($obj = $results->fetch_object()){
+          $refresh_token = $obj->refresh_token;
+          $redirect_uri = $obj->redirect_uri;
+          $client_id = $obj->client_id;
         }
     }
-    $refresh_token = "";
-    $client_id = "DERS_MONEY";
-    $redirect_uri = "https%3A%2F%2Fmoney.dersllc.com%3A8743";
-
     $curl = curl_init();
-
+    
+    $fields = array(
+	'grant_type' => urlencode('refresh_token'),
+	'refresh_token' => urlencode($refresh_token),
+	'access_type' => urlencode('offline'),
+	'code' => urlencode('code'),
+	'client_id' => urlencode($client_id),
+	'redirect_uri' => urlencode($redirect_uri)
+    );
+    foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
+    rtrim($fields_string, '&');
+    
     curl_setopt_array($curl, array(
       CURLOPT_URL => "https://api.tdameritrade.com/v1/oauth2/token",
       CURLOPT_RETURNTRANSFER => true,
@@ -25,7 +35,7 @@ function get_auth() {
       CURLOPT_TIMEOUT => 30,
       CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
       CURLOPT_CUSTOMREQUEST => "POST",
-      CURLOPT_POSTFIELDS => "grant_type=refresh_token&refresh_token=".$refresh_token."&access_type=offline&code=code&client_id=".$client_id."&redirect_uri=".$redirect_uri,
+      CURLOPT_POSTFIELDS => $fields_string,
       CURLOPT_HTTPHEADER => array(
         "Content-Type: application/x-www-form-urlencoded",
         "cache-control: no-cache"
