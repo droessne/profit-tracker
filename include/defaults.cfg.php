d5d1ac332b2a5e86efc981a69c1a8ecdf1c9be2d
Daniel Roessner <daniel.roessner@dersllc.com>
2019-04-19 21:59:03 +0000

    Update defaults.cfg.php

--- include/defaults.cfg.php
+++ include/defaults.cfg.php
@@ -1,5 +1,5 @@
 <?php
-$platforms = array("Alpha-9", "Straight Line Profits", "Money Calendar Pro", "Weekly Money Call", "The Money Zone", "Fast Fortune Club", "10 Minute Millionaire", "Stealth Profits Trader", "Seismic Profits Alert", "DERs");
+$platforms = array("Alpha-9", "Money Calendar Pro", "Weekly Money Call", "The Money Zone", "Fast Fortune Club", "DERs", "Straight Line Profits", "10 Minute Millionaire", "Stealth Profits Trader", "Seismic Profits Alert");
 $monthly_profit_percent_target = (.4);
 $monthly_profit_percent_to_keep = (.75);
 ?>
