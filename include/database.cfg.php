cafd95cd3dfba53660d106824cb98ce1b6844562
Daniel Roessner <daniel.roessner@dersllc.com>
2019-04-23 14:46:07 +0000

    Update database.cfg.php

--- include/database.cfg.php
+++ include/database.cfg.php
@@ -3,4 +3,6 @@ define("DB_HOST", "172.16.87.60");
 define("DB_NAME", "profit_tracker");
 define("DB_USER", "ders-address");
 define("DB_PASS", "DERS4me");
+$profits_table = "profits";
+$trades_table = "trades";
 ?>
