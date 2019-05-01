9e8fb304f924fc314a073d41a2fe9edd12a23d6b
Daniel Roessner <daniel.roessner@dersllc.com>
2019-05-01 16:07:38 +0000

    Update create_broker_db.php

--- install/create_broker_db.php
+++ install/create_broker_db.php
@@ -18,7 +18,47 @@ if ($dbconnection->connect_error) {
   $results1 = $dbconnection->query($sql1);
   if ($results1) {
     echo "Broker added.";
-    header("Location: {$_SERVER['HTTP_REFERER']}");
+    $sql2 = "CREATE TABLE IF NOT EXISTS `".$broker_trade_table_name."` (
+            `ID` int(11) NOT NULL AUTO_INCREMENT,
+            `executed_date` date NOT NULL,
+            `sell_by_date` date,
+            `type` enum('Entry','Exit') CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+            `symbol` varchar(10) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+            `trade_strategy` enum('Call','Call Spread','Put','Bull Put Spread','Bear Put Spread') CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+            `order_type` enum('Buy Open','Sell Close','Sell Open','Buy Close') CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+            `qty` int NOT NULL,
+            `expire_date` date NOT NULL,
+            `strike_price` float NOT NULL,
+            `executed_price` float NOT NULL,
+            `order_type2` enum('Buy Open','Sell Close','Sell Open','Buy Close') CHARACTER SET utf8 COLLATE utf8_unicode_ci,
+            `strike_price2` float,
+            `com_fee` float,
+            `total` float,
+            `platform` enum('Alpha-9','Money Calendar Pro','Straight Line Profits','Weekly Money Call','The Money Zone','Fast Fortune Club','10 Minute Millionaire','Stealth Profits Trader','Seismic Profits Alert','DERs') CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+            `mate_id` varchar(50),
+            PRIMARY KEY (`ID`)
+            ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12;";
+    if ($dbconnection->query($sql2) === TRUE) {
+       echo "Table ".$broker_trade_table_name." created successfully";
+       $sql3 = "CREATE TABLE IF NOT EXISTS `".$broker_trade_profit_name."` (
+                `ID` int(11) NOT NULL AUTO_INCREMENT,
+                `date` date NOT NULL,
+                `description` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+                `amount` float,
+                `platform` enum('Alpha-9','Money Calendar Pro','Straight Line Profits','Weekly Money Call','The Money Zone','Fast Fortune Club','10 Minute Millionaire','Stealth Profits Trader','Seismic Profits Alert','Deposit','Withdrawal') CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+                `entry_id` int(11),
+                `exit_id` varchar(50),
+                PRIMARY KEY (`ID`)
+                ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;";
+      if ($dbconnection->query($sql3) === TRUE) {
+       echo "Table ".$broker_trade_profit_name." created successfully";
+      } else {
+         echo "Error creating profits table: " . $dbconnection->error;
+      }
+    } else {
+       echo "Error creating trade table: " . $dbconnection->error;
+    }
+    #header("Location: {$_SERVER['HTTP_REFERER']}");
     die();
   } else {
     echo "Sorry, adding this broker failed. Please try again";
