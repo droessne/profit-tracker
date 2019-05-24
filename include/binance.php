8c0704284b8d55e115c20eed7bb51aaae0e0b59f
Daniel Roessner <daniel.roessner@dersllc.com>
2019-05-24 20:59:31 +0000

    Add new file

---
+++ include/binance.php
@@ -0,0 +1,10 @@
+<?php
+require '../vendor/autoload.php';
+
+$api = new Binance\API("rLtHblSweXAQ3mIhM8KV6L6B4ufxjphb6coBUWvqo53cl89RAIOehpzjvMh9ORcW","DzueI1FK56MjVFZV0G7vK7EpCtuy4YHdtDYrJquaR8dCT10YYWUgIK5t32hKPKp9");
+
+$price = $api->price("IOTXBTC");
+echo "Price of IOTX: {$price} BTC.".PHP_EOL;
+
+
+?>
