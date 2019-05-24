cd472c9b53e7e42e8d0b52648b5308e01c80cade
Daniel Roessner <daniel.roessner@dersllc.com>
2019-05-24 22:50:23 +0000

    Update binance.php

--- install/binance.php
+++ install/binance.php
@@ -1,5 +1,5 @@
 <?php
-require '../vendor/autoload.php';
+#require '../vendor/autoload.php';
 
 $api = new Binance\API("rLtHblSweXAQ3mIhM8KV6L6B4ufxjphb6coBUWvqo53cl89RAIOehpzjvMh9ORcW","DzueI1FK56MjVFZV0G7vK7EpCtuy4YHdtDYrJquaR8dCT10YYWUgIK5t32hKPKp9");
 
