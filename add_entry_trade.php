36dd6aad2a4c10c403dd49267eb93b2a1c30b21d
Daniel Roessner <daniel.roessner@dersllc.com>
2019-05-24 16:56:01 +0000

    Update add_entry_trade.php

--- add_entry_trade.php
+++ add_entry_trade.php
@@ -50,11 +50,11 @@
   <option value="Sell Open">Sell Open</option>
   <option value="Buy Close">Buy Close</option>
 </select></td>
-<td><input type="text" name="qty" value="" size="3"></td>
+<td><input type="text" name="qty" value="" size="15"></td>
 <td><input type="text" name="expire_date" value="<?php date_default_timezone_set("America/New_York"); echo date("Y-m-d"); ?>" size="10"></td>
-<td><input type="text" name="strike_price" value="" size="10"></td>
-<td><input type="text" name="executed_price" value="" size="10"></td>
-<td><input type="text" name="com_fee" value="" size="12"></td>
+<td><input type="text" name="strike_price" value="" size="15"></td>
+<td><input type="text" name="executed_price" value="" size="15"></td>
+<td><input type="text" name="com_fee" value="" size="15"></td>
 </tr>
 <tr>
 <td></td>
@@ -68,7 +68,7 @@
 </select></td>
 <td></td>
 <td></td>
-<td><input type="text" name="strike_price2" value="" size="10"></td>
+<td><input type="text" name="strike_price2" value="" size="15"></td>
 <td></td>
 <td></td>
 </tr>
