b6600c365be6d936983f4adf947195a884265203
Daniel Roessner <daniel.roessner@dersllc.com>
2019-06-03 18:19:15 +0000

    Update first_auth_db.php

--- first_auth_db.php
+++ first_auth_db.php
@@ -12,17 +12,17 @@ if ($dbconnection->connect_error) {
 
 
   $access_token = $data->access_token;
-  #echo $access_token;
-  #echo "<br>";
+  echo $access_token;
+  echo "<br>";
   $refresh_token = $data->refresh_token;
-  #echo $refresh_token;
-  #echo "<br>";
+  echo $refresh_token;
+  echo "<br>";
   $client_id = "DERS_MONEY1@AMER.OAUTHAP";
-  #echo $client_id;
-  #echo "<br>";
+  echo $client_id;
+  echo "<br>";
   $redirect_uri = "https%3A%2F%2Fapi.dersllc.com%3A8743";
-  #echo $redirect_uri;
-  #echo "<br>";
+  echo $redirect_uri;
+  echo "<br>";
 
   $sql = "INSERT INTO auth (ID, access_token, refresh_token, client_id, redirect_uri) VALUES(1, '".$access_token."', '".$refresh_token."', '".$client_id."', '".$redirect_uri."');";
   $results = $dbconnection->query($sql);
