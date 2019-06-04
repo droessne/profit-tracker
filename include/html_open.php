34df5ee23a0323c202b4c60bb690e26a5e90e7eb
Daniel Roessner <daniel.roessner@dersllc.com>
2019-06-04 14:30:33 +0000

    Add new file

---
+++ include/html_open.php
@@ -0,0 +1,11 @@
+<?php
+echo '<!DOCTYPE html>';
+echo '<html>';
+echo '<head>';
+echo '  <meta name="viewport" content="width=device-width, initial-scale=1">';
+echo '  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">';
+echo '  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>';
+echo '  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>';
+echo '</head>';
+echo '<body>';
+?>
