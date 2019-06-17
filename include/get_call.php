cfc57b8c6dc2af127bd4bce1744de31e49146a0e
Daniel Roessner <daniel.roessner@dersllc.com>
2019-06-17 22:20:44 +0000

    Update get_call.php

--- include/get_call.php
+++ include/get_call.php
@@ -66,8 +66,8 @@ function get_call($symbol, $strike, $expire_date){
   }
 }
 
-$results = get_call('BABA','220', '2019-07-19');
-print_r($results);
-echo $results['mark'];
+#$results = get_call('BABA','220', '2019-07-19');
+#print_r($results);
+#echo $results['mark'];
 
 ?>
