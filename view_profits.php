11f36ff720c7ae8ea53f5d93fbd2d1946fb51799
Daniel Roessner <daniel.roessner@dersllc.com>
2019-06-18 19:31:39 +0000

    Update view_profits.php

--- view_profits.php
+++ view_profits.php
@@ -44,43 +44,43 @@ function viewByPlatform($platform, $trades_table, $profits_table){
     echo "<th>".money_format($format, $total_balance)."</th>";
     echo "<th>$platform_percent</th></tr>";
     echo "</table><BR>";
-    echo "<table border=1 width=80%>";
-    echo "<tr>
-          <th><span style='font-size:.8em'>Date</span></th>
-          <th><span style='font-size:.8em'>Description</span></th>
-          <th><span style='font-size:.8em'>Amount</span></th>
-          <th><span style='font-size:.8em'>Balance</span></th>
-          <th><span style='font-size:.8em'>Actions</span></th>
-          </tr>";
-    $sql = "SELECT * FROM ".$profits_table." WHERE platform='".$platform."' ORDER BY date DESC;";
-    $results = $dbconnection->query($sql);
-    $count = 0;
-    $last_amount = 0;
-    $balance = 0;
-    while($obj = $results->fetch_object()){
-      if ( $count == 0 ) {
-        $balance = $total_balance;
-      } else {
-        $balance = ($balance - $last_amount);
-      }
-      echo "<tr>
-            <td align='center'><span style='font-size:.8em'>$obj->date</span></td>
-            <td align='center'><span style='font-size:.8em'>$obj->description</span></td>
-            <td align='center'><span style='font-size:.8em'>".money_format($format, $obj->amount)."</span></td>
-            <td align='center'><span style='font-size:.8em'>".money_format($format, $balance)."</span></td>
-            <td align='center'><table><tr>
-                <td><form method='POST' action='edit_profit.php'>
-                <input type='hidden' name='ID' value='$obj->ID'>
-                <button type='submit'>Edit</button></form></td>
-                <td valign='bottom'><form method='POST' action='delete_profit.php'>
-                <input type='hidden' name='ID' value='$obj->ID'>
-                <button type='submit'>Del</button></form></td></tr></table></td>
-            </tr>";
-      $last_amount = $obj->amount;
-      $count++;
-    }
-    $results->close();
-    unset($obj);
+    #echo "<table border=1 width=80%>";
+    #echo "<tr>
+    #      <th><span style='font-size:.8em'>Date</span></th>
+    #      <th><span style='font-size:.8em'>Description</span></th>
+    #      <th><span style='font-size:.8em'>Amount</span></th>
+    #      <th><span style='font-size:.8em'>Balance</span></th>
+    #      <th><span style='font-size:.8em'>Actions</span></th>
+    #      </tr>";
+    #$sql = "SELECT * FROM ".$profits_table." WHERE platform='".$platform."' ORDER BY date DESC;";
+    #$results = $dbconnection->query($sql);
+    #$count = 0;
+    #$last_amount = 0;
+    #$balance = 0;
+    #while($obj = $results->fetch_object()){
+    #  if ( $count == 0 ) {
+    #    $balance = $total_balance;
+    #  } else {
+    #    $balance = ($balance - $last_amount);
+    #  }
+    #  echo "<tr>
+    #        <td align='center'><span style='font-size:.8em'>$obj->date</span></td>
+    #        <td align='center'><span style='font-size:.8em'>$obj->description</span></td>
+    #        <td align='center'><span style='font-size:.8em'>".money_format($format, $obj->amount)."</span></td>
+    #        <td align='center'><span style='font-size:.8em'>".money_format($format, $balance)."</span></td>
+    #        <td align='center'><table><tr>
+    #            <td><form method='POST' action='edit_profit.php'>
+    #            <input type='hidden' name='ID' value='$obj->ID'>
+    #            <button type='submit'>Edit</button></form></td>
+    #            <td valign='bottom'><form method='POST' action='delete_profit.php'>
+    #            <input type='hidden' name='ID' value='$obj->ID'>
+    #            <button type='submit'>Del</button></form></td></tr></table></td>
+    #        </tr>";
+    #  $last_amount = $obj->amount;
+    #  $count++;
+    #}
+    #$results->close();
+    #unset($obj);
   }
   echo "</table></center>";
 }
