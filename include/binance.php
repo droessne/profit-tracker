6e5f461a4b86a4797baca73b51dea6b5cfc3f5a1
Daniel Roessner <daniel.roessner@dersllc.com>
2019-06-18 14:23:47 +0000

    Update binance.php

--- include/binance.php
+++ include/binance.php
@@ -4,7 +4,7 @@ function get_crypto($symbol){
   $api_key = "rLtHblSweXAQ3mIhM8KV6L6B4ufxjphb6coBUWvqo53cl89RAIOehpzjvMh9ORcW";
   $secret = "DzueI1FK56MjVFZV0G7vK7EpCtuy4YHdtDYrJquaR8dCT10YYWUgIK5t32hKPKp9";
   $signature = hash_hmac('sha256', $params, $secret);
-      
+
   $curl = curl_init();
 
   curl_setopt_array($curl, array(
@@ -21,10 +21,10 @@ function get_crypto($symbol){
       ),
     ));
 
-  $response = curl_exec($curl);
-  $err = curl_error($curl);
+  #$response = curl_exec($curl);
+  #$err = curl_error($curl);
 
-  curl_close($curl);
+  #curl_close($curl);
 
   if ($err) {
     echo "cURL Error #:" . $err;
@@ -43,7 +43,7 @@ function get_crypto($symbol){
     return $results;
   }
 }
-#$results = get_crypto('IOTXBTC');
+get_crypto('IOTXBTC');
 #print_r($results);
 #echo $results['mark'];
 ?>
