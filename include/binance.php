47035de644ea2a75fe201dbea1acdea69b9df2b0
Daniel Roessner <daniel.roessner@dersllc.com>
2019-06-18 14:12:27 +0000

    Update binance.php

--- include/binance.php
+++ include/binance.php
@@ -1,10 +1,55 @@
 <?php
-require '../vendor/autoload.php';
+#require '../vendor/autoload.php';
+#$api = new Binance\API("rLtHblSweXAQ3mIhM8KV6L6B4ufxjphb6coBUWvqo53cl89RAIOehpzjvMh9ORcW","DzueI1FK56MjVFZV0G7vK7EpCtuy4YHdtDYrJquaR8dCT10YYWUgIK5t32hKPKp9");
+#$price = $api->price("IOTXBTC");
+#echo "Price of IOTX: {$price} BTC.".PHP_EOL;
+?>
+<?php
+function get_crypto($symbol){
+  $params = "symbol=".$symbol;
+  $api_key = "rLtHblSweXAQ3mIhM8KV6L6B4ufxjphb6coBUWvqo53cl89RAIOehpzjvMh9ORcW";
+  $secret = "DzueI1FK56MjVFZV0G7vK7EpCtuy4YHdtDYrJquaR8dCT10YYWUgIK5t32hKPKp9";
+  $signature = hash_hmac('sha256', $params, $secret);
+      
+    $curl = curl_init();
 
-$api = new Binance\API("rLtHblSweXAQ3mIhM8KV6L6B4ufxjphb6coBUWvqo53cl89RAIOehpzjvMh9ORcW","DzueI1FK56MjVFZV0G7vK7EpCtuy4YHdtDYrJquaR8dCT10YYWUgIK5t32hKPKp9");
+    curl_setopt_array($curl, array(
+      CURLOPT_URL => "https://api.binance.com/api/v3/ticker/price",
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_ENCODING => "",
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 30,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => "GET",
+      CURLOPT_POSTFIELDS => $params."&signature=".$signature,
+      CURLOPT_HTTPHEADER => array(
+        "X-MBX-APIKEY: ".$api_key
+      ),
+    ));
 
-$price = $api->price("IOTXBTC");
-echo "Price of IOTX: {$price} BTC.".PHP_EOL;
+  $response = curl_exec($curl);
+  $err = curl_error($curl);
 
+  curl_close($curl);
 
+  if ($err) {
+    echo "cURL Error #:" . $err;
+  } else {
+    echo $response;
+    #$data = json_decode($response);
+    $mark = $data->$symbol->mark;
+    #$results = [
+    #  "last" => $last,
+    #  "mark" => $mark,
+    #  "volatility" => $volatility,
+    #  "div_yield" => $div_yield,
+    #  "tot_vol" => $tot_vol,
+    #  "d_2_ex" => 999
+    #];
+    return $results;
+  }
+}
+$results = get_crypto('IOTXBTC');
+print_r($results);
+#echo $results['mark'];
 ?>
