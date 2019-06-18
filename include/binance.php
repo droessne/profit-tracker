361e5d70e3eb087362b4190affb2bee242df8ffc
Daniel Roessner <daniel.roessner@dersllc.com>
2019-06-18 14:18:43 +0000

    Update binance.php

--- include/binance.php
+++ include/binance.php
@@ -3,7 +3,7 @@ function get_crypto($symbol){
   $params = "symbol=".$symbol;
   $api_key = "rLtHblSweXAQ3mIhM8KV6L6B4ufxjphb6coBUWvqo53cl89RAIOehpzjvMh9ORcW";
   $secret = "DzueI1FK56MjVFZV0G7vK7EpCtuy4YHdtDYrJquaR8dCT10YYWUgIK5t32hKPKp9";
-  $signature = hash_hmac('sha256', $params, $secret);
+  #$signature = hash_hmac('sha256', $params, $secret);
       
   $curl = curl_init();
 
