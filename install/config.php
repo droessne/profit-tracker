bdffc7fe0507bf632fef1462f5e496e3b4940d36
Daniel Roessner <daniel.roessner@dersllc.com>
2019-06-21 13:19:54 +0000

    Update config.php

--- install/config.php
+++ install/config.php
@@ -2,7 +2,7 @@
 <title> Profit Tracker </title>
 <head>
 <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
-<script> src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"</script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 </head>
 <body>
 <H1> Settings </H1>
