527755c4472d92bdb12f6579fcca81bc198a289e
Daniel Roessner <daniel.roessner@dersllc.com>
2019-07-08 19:42:28 +0000

    Update cron.sh

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -7,9 +7,9 @@ user="dersllcc_money"
 psw="DERS4me"
 database="dersllcc_profit_tracker"
 query="select broker_id from brokers"
-active_broker=$(echo "SELECT active_broker_id FROM defaults" | mysql -h ${host} -u${user} -p${psw} ${database} | tail -1)
+active_broker=$(echo "SELECT active_broker_id FROM defaults" | /usr/local/bin/mysql -h ${host} -u${user} -p${psw} ${database} | tail -1)
 
-cd /tmp/profit-tracker/
+#cd /tmp/profit-tracker/
 #sed -i 's/127.0.0.1/gn400.whpservers.com/' include/database.cfg.php
 
 while read line
@@ -23,9 +23,9 @@ done < <(mysql -h ${host} -u${user} -p${psw} ${database} -e "${query}")
 for b in $brokers
 do
   query2="UPDATE defaults SET active_broker_id = '$b' WHERE id = 1"
-  mysql -h ${host} -u${user} -p${psw} ${database} -e "${query2}"
-  php71 view_open2.php
+  /usr/local/bin/mysql -h ${host} -u${user} -p${psw} ${database} -e "${query2}"
+  /usr/local/bin/php /home2/dersllcc/public_html/home/uploads/money/view_open2.php
 done
 
 query2="UPDATE defaults SET active_broker_id = '$active_broker' WHERE id = 1"
-mysql -h ${host} -u${user} -p${psw} ${database} -e "${query2}"
+/usr/local/bin/mysql -h ${host} -u${user} -p${psw} ${database} -e "${query2}"
